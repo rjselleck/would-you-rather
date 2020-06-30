6195d56dc4ed4e8c76dd7de95bf87ddfb4db1a42
Robert Selleck <selleck1@lsu.edu>
2020-06-30 08:04:43 -0500

    Data initialized in App

---
+++ src/actions/shared.js
@@ -0,0 +1,18 @@
+import { getInitialData } from '../utils/api.js'
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'sarahedo'
+
+// Action Creators
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
+}
---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>
+        <h1>Would You Rather</h1>
+      </div>
+    )
+  }
+}
+
+export default connect()(App)
